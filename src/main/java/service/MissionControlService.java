4d38a44152beb3a7f785d58581ae3923a375942b
Robert Vihodita <robert.vihodita@stud.ubbcluj.ro>
2026-02-17 10:50:20 +0200

    task 5 completed

--- src/main/java/service/MissionControlService.java
+++ src/main/java/service/MissionControlService.java
@@ -92,6 +92,30 @@ public class MissionControlService {
         pw.close();
     }
 
+    // TASK 5
+    public int computePoints(MissionEvent e) {
+
+        switch (e.getType()) {
+
+            case EVA:
+                return e.getBasePoints() + 2 * e.getDay();
+
+            case SYSTEM_FAILURE:
+                return e.getBasePoints() - 3 - e.getDay();
+
+            case SCIENCE:
+                return e.getBasePoints() + e.getDay() % 4;
+
+            case MEDICAL:
+                return e.getBasePoints() - 2 * (e.getDay() % 3);
+
+            case COMMUNICATION:
+                return e.getBasePoints() + 5;
+        }
+
+        return 0;
+    }
+
 
 }
 
